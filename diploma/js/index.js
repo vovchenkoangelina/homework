5b8351131241c70f378fc526fe844444b9768b93
vovchenkoangelina <vovchenko.angelina@gmail.com>
2025-12-09 03:00:35 +0700

    Add files via upload

--- diploma/js/index.js
+++ diploma/js/index.js
@@ -1,9 +1,14 @@
 import { newsSlider } from "./slider.js";
 import { vacanciesSlider } from "./slider.js";
+import Modal from "./modal.js";
 
 try {
     newsSlider();
     vacanciesSlider();
+    new Modal({
+        PAGE_BODY: "page__body",
+        PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+    });
 }
 catch (error) {
     console.error(error);
