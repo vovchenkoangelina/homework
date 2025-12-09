b26d63c03c809422692028ada9e5637897076da9
vovchenkoangelina <vovchenko.angelina@gmail.com>
2025-12-10 03:31:37 +0700

    Add files via upload

--- diploma/js/slider.js
+++ diploma/js/slider.js
@@ -1,59 +1,46 @@
 export const newsSlider = () => {
     new Swiper('.news__slider', {
-    slidesPerView: 3,      
-    spaceBetween: 20,      
-    slidesPerGroup: 1,     
-    loop: false,          
-
-    navigation: {
-        prevEl: ".news__slider-button--prev",
-        nextEl: ".news__slider-button--next",
-    },
-
-    // отображение слайдов для устройств с меньшим экраном
-    breakpoints: {
-        0: {            
         slidesPerView: 1,
         spaceBetween: 16,
+        slidesPerGroup: 1,
+        loop: false,
+        navigation: {
+            prevEl: ".news__slider-button--prev",
+            nextEl: ".news__slider-button--next",
         },
-        768: {      
-        slidesPerView: 2,
-        spaceBetween: 20,
+
+        // отображение слайдов для устройств с меньшим экраном
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 16,
+            },
+            500: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            }, 1200: {
+                slidesPerView: 3,
+                spaceBetween: 24,
+            },
         },
-        1024: {      
-        slidesPerView: 3,
-        spaceBetween: 24,
-        },
-    },
     });
 };
 
 export const vacanciesSlider = () => {
     new Swiper('.vacancies__slider', {
-    slidesPerView: 3,      
-    spaceBetween: 20,      
-    slidesPerGroup: 1,     
-    loop: false,          
-
-    navigation: {
-        prevEl: ".vacancies__slider-button--prev",
-        nextEl: ".vacancies__slider-button--next",
-    },
-
-    // отображение слайдов для устройств с меньшим экраном
-    breakpoints: {
-        0: {            
         slidesPerView: 1,
         spaceBetween: 16,
+        slidesPerGroup: 1,
+        loop: false,
+        navigation: {
+            prevEl: '.vacancies__slider-button--prev',
+            nextEl: '.vacancies__slider-button--next',
         },
-        768: {      
-        slidesPerView: 2,
-        spaceBetween: 20,
+        breakpoints: {
+            0: { slidesPerView: 1, spaceBetween: 16, },
+            500: { slidesPerView: 2, spaceBetween: 20, },
+            1200: { slidesPerView: 3, spaceBetween: 24, },
         },
-        1024: {      
-        slidesPerView: 3,
-        spaceBetween: 24,
-        },
-    },
     });
 };
+
