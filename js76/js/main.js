8bb7e85e07bae804705de86e2981e09804871a55
vovchenkoangelina <vovchenko.angelina@gmail.com>
2025-12-20 02:37:28 +0300

    Add files via upload

---
+++ js76/js/main.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const todoKeys = {
+  id: "id",
+  text: "text",
+  is_completed: "is_completed",
+};
+
+const todos = [];
+
+const errTodoNotFound = todoId => `Todo with id ${todoId} not found`;
+
+const getNewTodoId = todos =>
+  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, text) => {
+  const newTodo = {
+    [todoKeys.id]: getNewTodoId(todos),
+    [todoKeys.text]: text,
+    [todoKeys.is_completed]: false,
+  };
+  todos.push(newTodo);
+  return newTodo;
+};
+
+const completeTodoById = (todos, todoId) => {
+  const todo = todos.find(todo => todo[todoKeys.id] === todoId);
+
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
+  }
+  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+  return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+  const todoIndex = todos.findIndex(todo => todo[todoKeys.id] === todoId);
+  if (todoIndex === -1) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todos.splice(todoIndex, 1);
+  return todos;
+};
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const todosList = document.querySelector(".todos");
+
+const createTodoElement = (text) => {
+  const todoElement = document.createElement("li");
+  todoElement.classList.add("todo");
+  todoElement.innerHTML = `
+          <div class="todo-text">${text}</div>
+          <div class="todo-actions">
+            <button class="button-complete button">&#10004;</button>
+            <button class="button-delete button">&#10006;</button>
+          </div>
+  `;
+  todosList.append(todoElement);
+}
+
+const handleCreateTodo = (todos, text) => {
+  createTodo(todos, text);
+  createTodoElement(text);
+}
